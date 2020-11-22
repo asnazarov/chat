e4c6278f605a778f988af2e062297dc37104d074
Acer Laptop <asn.nazarov.asn@gmail.com>
2020-11-22 17:14:58 +0300

    'nodeSass'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './styl.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Title</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
